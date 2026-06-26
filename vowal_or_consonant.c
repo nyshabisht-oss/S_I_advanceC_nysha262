e7d521a5769b80964b6e08d54a314f045f0dee45
nyshabisht-oss <nyshabisht@gmail.com>
2026-06-26 22:08:08 +0530

    Add files via upload

---
+++ vowal_or_consonant.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    char character;
+    printf("Enter a Character: ");
+    scanf("%c",&character);
+
+    if(character=='a' || character=='e' || character=='i' || character=='o' || character=='u'
+    || character=='A' || character=='E' || character=='I' || character=='O' || character=='U')
+    {
+        printf("Character is a vowel");
+    }
+    else
+    {
+        printf("Character is a consonant or symbol");
+    }
+    return 0;
+}
